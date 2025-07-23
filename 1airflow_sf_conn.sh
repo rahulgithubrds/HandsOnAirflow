992ecf7ee05ff2c095a45eaf7123c85a2c2da9a7
Rahul <73097970+rahulgithubrds@users.noreply.github.com>
2025-07-23 22:39:38 +0530

    Update 1airflow_sf_conn.sh

--- 1airflow_sf_conn.sh
+++ 1airflow_sf_conn.sh
@@ -1,9 +1,10 @@
-#Bash
+# Bash
 # This code will create an airflow connection with Snowflake
+# Paramters values used here are just for demonstrations
 
 airflow connections add snowflake_default    \ 
   --conn-type snowflake \
-  --conn-login <your_snowflake_username> \
-  --conn-password '<your_snoflake_password'> \
+  --conn-login <snowflake_username> \
+  --conn-password '<snoflake_password'> \
   --conn-schema PUBLIC \
   --conn-extra '{"account": "PQVEFAQ-AN22787", "warehouse": "COMPUTE_WH","database": "QUICK_PRACTICE_DB","role": "ACCOUNTADMIN"}'
